b2438733db754f3c7f3eb69f7e17dc059a32b221
Noahbds <noahbds@users.noreply.github.com>
2025-01-21 21:53:59 +0100

    make the extension better

--- content.js
+++ content.js
@@ -1,57 +1,52 @@
-(function () {
+(function() {
     'use strict';
 
-    const addRandomAnimeButton = () => {
-        const headerActions = document.querySelector('.header-actions ul.erc-user-actions');
+    const iconUrl = 'https://github.com/noahbds/crunchyroll-random-anime/blob/main/icons/randomimg.png?raw=true';
 
-        if (headerActions) {
-            const randomButtonLi = document.createElement('li');
-            randomButtonLi.className = 'user-actions-item';
+    function createRandomAnimeButton() {
+        const headerActions = document.querySelector('ul.erc-user-actions');
+        if (!headerActions) return;
 
-            const randomButton = document.createElement('a');
-            randomButton.href = 'https://www.crunchyroll.com/fr/random/anime?random_ref=topbar'; // still works so it's easy
-            randomButton.className = 'erc-header-tile state-icon-only erc-random-header-button';
-            randomButton.setAttribute('tabindex', '0');
-            randomButton.setAttribute('data-t', 'header-tile');
+        const randomButtonLi = document.createElement('li');
+        randomButtonLi.className = 'user-actions-item';
 
-            const buttonDiv = document.createElement('div');
-            buttonDiv.className = 'erc-header-svg';
+        const randomButton = document.createElement('a');
+        randomButton.href = 'https://www.crunchyroll.com/fr/random/anime?random_ref=topbar';
+        randomButton.className = 'erc-header-tile state-icon-only erc-random-header-button';
+        randomButton.tabIndex = 0;
+        randomButton.dataset.t = 'header-tile';
 
-            const buttonIcon = document.createElement('img');
-            buttonIcon.src = chrome.runtime.getURL("icons/randomimg.png"); // original icon
-            buttonIcon.alt = 'Random Anime Icon';
-            buttonIcon.style.width = '24px';
-            buttonIcon.style.height = '24px';
+        const buttonDiv = document.createElement('div');
+        buttonDiv.className = 'erc-header-svg';
 
-            buttonDiv.appendChild(buttonIcon);
+        const buttonIcon = document.createElement('img');
+        buttonIcon.src = iconUrl;
+        buttonIcon.alt = 'Random Anime Icon';
+        buttonIcon.style.cssText = 'width: 24px; height: 24px; display: inline-block; vertical-align: middle; margin-right: 8px;';
 
-            const buttonText = document.createElement('span');
-            buttonText.className = 'text--gq6o- text--is-l--iccTo';
-            buttonText.textContent = 'Random Anime';
+        const buttonText = document.createElement('span');
+        buttonText.className = 'text--gq6o- text--is-l--iccTo';
+        buttonText.textContent = 'Random Anime';
 
-            randomButton.appendChild(buttonDiv);
-            randomButton.appendChild(buttonText);
-            randomButtonLi.appendChild(randomButton);
+        buttonDiv.appendChild(buttonIcon);
+        randomButton.appendChild(buttonDiv);
+        randomButton.appendChild(buttonText);
+        randomButtonLi.appendChild(randomButton);
 
-            const searchButton = document.querySelector('.erc-search-header-button');
-            if (searchButton) {
-                headerActions.insertBefore(randomButtonLi, searchButton.parentNode);
-            }
+        const searchButton = document.querySelector('.erc-search-header-button-old');
+        if (searchButton) {
+            headerActions.insertBefore(randomButtonLi, searchButton.parentNode);
+        } else {
+            headerActions.appendChild(randomButtonLi);
         }
-    };
+    }
 
-    const observer = new MutationObserver((mutationsList) => {
-        for (const mutation of mutationsList) {
-            if (document.querySelector('.header-actions ul.erc-user-actions')) {
-                addRandomAnimeButton();
-                observer.disconnect();
-                break;
-            }
+    const observer = new MutationObserver(() => {
+        if (document.querySelector('ul.erc-user-actions')) {
+            createRandomAnimeButton();
+            observer.disconnect();
         }
     });
 
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true
-    });
+    observer.observe(document.body, { childList: true, subtree: true });
 })();
